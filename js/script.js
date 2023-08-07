cd480690d3f048d77a404c0dae3d27a29f55ada3
preich-work <preich_work@yahoo.com>
2023-08-07 15:12:37 -0400

    Initialized git

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var button = document.querySelector(".got-this");
+var modal = document.querySelector(".modal");
+var modalx = document.querySelector(".modal-x");
+
+button.addEventListener ("click", function ()
+{
+    modal.classList.add("show-modal");
+
+button.innerText = "You've got this!";
+});
+
+modalx.addEventListener("click", function ()
+{ modal.classList.remove("show-modal");
+    button.innerText = "Who's got this?";
+});
